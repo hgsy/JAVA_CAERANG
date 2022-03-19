dc1d30259b53917f67b1957ce7c13da1dacfc950
Hwi Geun Hwang <57276270+hgsy@users.noreply.github.com>
2022-03-20 07:31:55 +0900

    Create Backjoon_2588.java

---
+++ Backjoon_2588.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+/*
+ * 1차 오답 : 일, 십, 백의 자리 변수 반대로 대입함.
+ * 2차 정답.
+ */
+public class Backjoon_2588_2 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int l1 = input.nextInt();
+		int l2 = input.nextInt();
+		int value = 0;
+		
+		int l2_100 = l2/100;
+		value = l2%100;
+		int l2_10 = value/10;
+		value = l2%10;
+		int l2_1 = value;
+		
+		int l3 = l1 * l2_1;
+		int l4 = l1 * l2_10;
+		int l5 = l1 * l2_100;
+		int result = l3 + (10 * l4) + (100 * l5);
+		
+		System.out.println(l3);
+		System.out.println(l4);
+		System.out.println(l5);
+		System.out.println(result);
+		input.close();
+	}
+
+}
