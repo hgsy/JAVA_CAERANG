e0ccb22d476f078c0e9b4570d91e623775d944af
Hwi Geun Hwang <57276270+hgsy@users.noreply.github.com>
2022-03-23 04:05:29 +0900

    Update BitTest_1.java

--- TestFolder/BitTest_1.java
+++ TestFolder/BitTest_1.java
@@ -6,7 +6,7 @@ public class BitTest_1 {
 		
 		System.out.print("정수를 입력하세요 : ");
 		int input = s.nextInt();
-		int reverse = ~input;
+		int reverse = ~input; //결과 비교용
 		int mask = 1;
 		int result;
 		
@@ -17,9 +17,10 @@ public class BitTest_1 {
 			result = input >> i & mask;
 			System.out.printf("%d",result);
 		}
+		
 		/* 기억 못할까봐 쓰는 진행 상황
 		 * 0 생략안하고 출력까진 완료함. 연산자 중 ~ 사용하지 않고 뒤집기위해 ^
-     * 비트 비교하여 서로 다르면 참이 나오는 ^ 사용하려 했으나 결과가 1 또는 0 이 아닌 3, 4 등이 나와서 일단 보류
+     		 * 비트 비교하여 서로 다르면 참이 나오는 ^ 사용하려 했으나 결과가 1 또는 0 이 아닌 3, 4 등이 나와서 일단 보류
 		 */
 		
 		System.out.println("\n----------------------------");
